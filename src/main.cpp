39f872be75de2839a6718e48745bec36f33abdcc
Luis Ramón López <lrlopez@gmail.com>
2021-05-12 09:10:49 +0200

    gui: Refactorización de la pantalla de arranque

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,15 @@ void espi_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *col
 }
 
 // Pantallas
-static lv_obj_t *scrSplash;     // Pantalla de inicio (splash screen)
-static lv_obj_t *scrMain;       // Pantalla principal de lectura de código
-static lv_obj_t *scrConfig;     // Pantalla de configuración
+static lv_obj_t * scr_splash;     // Pantalla de inicio (splash screen)
+static lv_obj_t * lbl_estado_splash;
+
+static lv_obj_t * scr_main;       // Pantalla principal de lectura de código
+static lv_obj_t * scr_config;     // Pantalla de configuración
+
+void create_scr_splash();
+
+void initialize_gui();
 
 void setup() {
 
@@ -46,6 +52,15 @@ void setup() {
     tft.begin();            /* Inicializar TFT */
     tft.setRotation(3);     /* Orientación horizontal */
 
+    // Inicializar GUI
+    initialize_gui();
+
+    // Crear pantalla de arranque y cambiar a ella
+    create_scr_splash();
+    lv_scr_load(scr_splash);
+}
+
+void initialize_gui() {
     lv_init();
 
     // Inicializar buffers del GUI
@@ -64,18 +79,31 @@ void setup() {
     disp_drv.flush_cb = espi_disp_flush;
     disp_drv.draw_buf = &disp_buf;
     lv_disp_drv_register(&disp_drv);
+}
 
+void create_scr_splash() {
     // CREAR PANTALLA DE ARRANQUE (splash screen)
-    scrSplash = lv_obj_create(nullptr, nullptr);
+    scr_splash = lv_obj_create(nullptr, nullptr);
     static lv_style_t styleFondoBlanco;
-    lv_obj_set_style_bg_color(scrSplash, 0, LV_STATE_DEFAULT, LV_COLOR_MAKE(255, 255, 255));
+    lv_obj_set_style_bg_color(scr_splash, 0, LV_STATE_DEFAULT, LV_COLOR_MAKE(255, 255, 255));
 
+    // Logo de la CED centrado
     LV_IMG_DECLARE(logo_ced);
-    lv_obj_t * img = lv_img_create(scrSplash, nullptr);
+    lv_obj_t * img = lv_img_create(scr_splash, nullptr);
     lv_img_set_src(img, &logo_ced);
     lv_obj_align(img, nullptr, LV_ALIGN_CENTER, 0, 0);
 
-    lv_scr_load(scrSplash);
+    // Spinner para indicar operación en progreso en la esquina inferior derecha
+    lv_obj_t * spinner = lv_spinner_create(scr_splash, 1000, 45);
+    lv_obj_set_size(spinner, 50, 50);
+    lv_obj_align(spinner, nullptr, LV_ALIGN_IN_BOTTOM_RIGHT, -10, -10);
+
+    // Etiqueta de estado actual centrada en la parte superior
+    lbl_estado_splash = lv_label_create(scr_splash, nullptr);
+    lv_label_set_text(lbl_estado_splash, "Inicializando...");
+    lv_obj_set_style_text_align(lbl_estado_splash, LV_PART_MAIN, LV_STATE_DEFAULT, LV_TEXT_ALIGN_CENTER);
+    lv_obj_set_style_text_font(lbl_estado_splash, LV_PART_MAIN, LV_STATE_DEFAULT, &lv_font_montserrat_24);
+    lv_obj_align(lbl_estado_splash, nullptr, LV_ALIGN_IN_TOP_MID, 0, 15);
 }
 
 void loop() {
