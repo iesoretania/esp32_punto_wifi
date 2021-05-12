11b872435e3db978c6ded55524088827a75aaad4
Luis Ramón López <lrlopez@gmail.com>
2021-05-12 21:05:58 +0200

    net: Inicialización de hora y comprobación de acceso a Séneca

--- include/punto_control.h
+++ include/punto_control.h
@@ -2,5 +2,7 @@
 #define ESP32_PUNTO_WIFI_PUNTO_CONTROL_H
 
 #define PUNTO_CONTROL_VERSION "0.9"
+#define PUNTO_CONTROL_HUSO_HORARIO "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00"
+#define PUNTO_CONTROL_URL "https://seneca.juntadeandalucia.es/seneca/controldepresencia/ControlPresencia.jsp"
 
 #endif //ESP32_PUNTO_WIFI_PUNTO_CONTROL_H
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <WiFi.h>
+#include <lwip/apps/sntp.h>
+#include <HTTPClient.h>
 
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 320
@@ -31,6 +33,7 @@ void espi_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *col
 
 // Pantallas
 static lv_obj_t * scr_splash;     // Pantalla de inicio (splash screen)
+static lv_obj_t * lbl_ip_splash;
 static lv_obj_t * lbl_estado_splash;
 
 static lv_obj_t * scr_main;       // Pantalla principal de lectura de código
@@ -50,18 +53,73 @@ void set_estado_splash(const char * string) {
     lv_obj_align(lbl_estado_splash, nullptr, LV_ALIGN_IN_TOP_MID, 0, 15);
 }
 
+void set_ip_splash_format(const char * string, const char * p) {
+    lv_label_set_text_fmt(lbl_ip_splash, string, p);
+    lv_obj_align(lbl_ip_splash, nullptr, LV_ALIGN_IN_BOTTOM_MID, 0, -15);
+}
+
+void set_ip_splash(const char * string) {
+    lv_label_set_text(lbl_ip_splash, string);
+    lv_obj_align(lbl_ip_splash, nullptr, LV_ALIGN_IN_BOTTOM_MID, 0, -15);
+}
+
 void task_wifi_connection(lv_timer_t * timer) {
+    static enum { CONNECTING, NTP, CHECKING, CHECK_RETRY, WAITING, DONE } state = CONNECTING;
     static int cuenta = 0;
-    static int connecting = true;
+    static HTTPClient client;
+    int code;
 
-    if (!connecting) return;
-
-    if (WiFi.status() == WL_CONNECTED) {
-        connecting = false;
-        set_estado_splash_format("Conectado: IP = %s", WiFi.localIP().toString().c_str());
-        lv_timer_del(timer);
-    } else {
-        cuenta++;
+    switch (state) {
+        case DONE:
+            lv_timer_del(timer);
+            return;
+        case CONNECTING:
+            if (WiFi.status() == WL_CONNECTED) {
+                state = NTP;
+                set_ip_splash_format("IP: %s", WiFi.localIP().toString().c_str());
+                set_estado_splash("Ajustando hora...");
+                setenv("TZ", PUNTO_CONTROL_HUSO_HORARIO, 1);
+                tzset();
+                sntp_setoperatingmode(SNTP_OPMODE_POLL);
+                sntp_setservername(0, "pool.ntp.org");
+                sntp_init();
+            } else {
+                cuenta++;
+            }
+            break;
+        case NTP:
+            if (sntp_get_sync_status() == SNTP_SYNC_STATUS_COMPLETED) {
+                state = DONE;
+            }
+            state = CHECKING;
+            cuenta = 0;
+            break;
+        case CHECKING:
+            set_estado_splash("Comprobando acceso a Séneca...");
+            client.begin(PUNTO_CONTROL_URL);
+            client.setReuse(false);
+            code = client.GET();
+            if (code != 200) {
+                set_estado_splash("Error de acceso. Reintentando");
+                state = CHECK_RETRY;
+            } else {
+                set_estado_splash("Acceso a Séneca confirmado");
+                state = WAITING;
+            }
+            client.end();
+            cuenta = 0;
+            break;
+        case CHECK_RETRY:
+            cuenta++;
+            if (cuenta == 20) {
+                state = CHECKING;
+            }
+            break;
+        case WAITING:
+            cuenta++;
+            if (cuenta == 20) {
+                state = DONE;
+            }
     }
 }
 
@@ -140,6 +198,12 @@ void create_scr_splash() {
     lv_obj_set_style_text_font(lbl_estado_splash, LV_PART_MAIN, LV_STATE_DEFAULT, &lv_font_montserrat_24);
     lv_obj_set_style_text_align(lbl_estado_splash, LV_PART_MAIN, LV_STATE_DEFAULT, LV_TEXT_ALIGN_CENTER);
     set_estado_splash("Inicializando...");
+
+    // Etiqueta de dirección IP actual centrada en la parte inferior
+    lbl_ip_splash = lv_label_create(scr_splash, nullptr);
+    lv_obj_set_style_text_font(lbl_ip_splash, LV_PART_MAIN, LV_STATE_DEFAULT, &lv_font_montserrat_18);
+    lv_obj_set_style_text_align(lbl_ip_splash, LV_PART_MAIN, LV_STATE_DEFAULT, LV_TEXT_ALIGN_CENTER);
+    set_ip_splash("Esperando configuración");
 
     // Etiqueta de versión del software en la esquina inferior izquierda
     lv_obj_t * lbl_version = lv_label_create(scr_splash, nullptr);
