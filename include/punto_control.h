46e8a24c32f4082a5087e2894619de50143c770a
Luis Ramón López <lrlopez@gmail.com>
2022-01-28 13:29:51 +0100

    Versión 3.1.8

--- include/punto_control.h
+++ include/punto_control.h
@@ -1,7 +1,7 @@
 #ifndef ESP32_PUNTO_WIFI_PUNTO_CONTROL_H
 #define ESP32_PUNTO_WIFI_PUNTO_CONTROL_H
 
-#define PUNTO_CONTROL_VERSION "v3.1.7"
+#define PUNTO_CONTROL_VERSION "v3.1.8"
 #define PUNTO_CONTROL_HUSO_HORARIO "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00"
 #define PUNTO_CONTROL_URL "https://seneca.juntadeandalucia.es/seneca/controldepresencia/ControlPresencia.jsp"
 #define PUNTO_CONTROL_LOGIN_URL "https://seneca.juntadeandalucia.es/seneca/controldepresencia/ComprobarUsuarioExt.jsp"
